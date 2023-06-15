eb8e88905dd111259f061ed2fe1bc832451731f1
Tedi Papajorgji <tedi.papajorgji@hotmail.com>
2023-06-15 03:32:05 -0400

    experiment with cache dir

--- g2pw/api.py
+++ g2pw/api.py
@@ -84,7 +84,7 @@ class G2PWConverter:
 
         self.device = torch.device('cuda' if use_cuda else 'cpu')
 
-        self.tokenizer = BertTokenizer.from_pretrained(self.model_source, local_files_only=True, cache_dir="saved_models")
+        self.tokenizer = BertTokenizer.from_pretrained(self.model_source, local_files_only=True)
 
         polyphonic_chars_path = os.path.join(model_dir, 'POLYPHONIC_CHARS.txt')
         monophonic_chars_path = os.path.join(model_dir, 'MONOPHONIC_CHARS.txt')
